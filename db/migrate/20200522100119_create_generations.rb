88ed281979752ed032b0d13c55f045a6b7784911
Ash Tyndall <git@atyndall.net>
2020-05-23 13:32:04 +0800

    Set broker default to 2

--- db/migrate/20200522100119_create_generations.rb
+++ db/migrate/20200522100119_create_generations.rb
@@ -2,7 +2,7 @@ class CreateGenerations < ActiveRecord::Migration[6.0]
   def change
     create_table :generations do |t|
       t.belongs_to :world, null: false, foreign_key: true
-      t.integer :broker_skill, limit: 1, null: false
+      t.integer :broker_skill, default: 2, limit: 1, null: false
       t.json :result, null: false
 
       t.timestamps
