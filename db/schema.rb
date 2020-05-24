bdfabe686868b06bc813ef35c87613b270460d44
Ash Tyndall <git@atyndall.net>
2020-05-24 17:17:28 +0800

    Reorder migrations so they work

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_24_074830) do
+ActiveRecord::Schema.define(version: 2020_05_24_090119) do
 
   create_table "generations", force: :cascade do |t|
     t.integer "world_id", null: false
